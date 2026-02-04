29c8e3d43427c3c720d8341e70049127e7a8e6ae
SERDOTTE <rodrigo.serdotte@gmail.com>
2026-02-03 22:50:24 -0300

    14 - ajustes no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-// Usamos 'as any' para evitar que o TS barre propriedades 
-// que ele ainda não conhece na versão 16
-const nextConfig: any = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  devIndicators: false,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
